2672daaedaaee973cc078053c0e5e0b5d2fd1bb8
Huzaifa <huzaifakashif2003@gmail.com>
2023-10-20 18:38:36 +0500

    promt the user to enter values added

--- airDistance.c
+++ airDistance.c
@@ -4,6 +4,18 @@
 int main()
 {
     double lat1,lon1,lat2,lon2;
+
+    printf("Enter the coordinates of the origin:\n");
+    printf("Latitude (in degrees): ");
+        scanf("%lf", &lat1);
+    printf("Longitude (in degrees): ");
+        scanf("%lf", &lon1);
     
+    printf("Enter the coordinates of the destination:\n");
+    printf("Latitude (in degrees): ");
+        scanf("%lf", &lat2);
+    printf("Longitude (in degrees): ");
+        scanf("%lf", &lon2);
+
     return 0;
 }
